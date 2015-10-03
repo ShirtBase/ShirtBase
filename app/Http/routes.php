8a32f3ab2b4cd924a4fd36cc14d2bcfad4ea4edd
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-10-03 20:03:28 +0200

    Add routes for shirts API endpoint, satisfy tests.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => '/api'], function() {
+    Route::group(['prefix' => 'shirts'], function() {
+       Route::get('/', function() {
+           return 'Works!';
+       }) ;
+    });
+});
