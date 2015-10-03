9b58c84ebcd5b5e5c5ca5feda038b1d8f5fdc1a5
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-10-03 19:16:35 +0200

    Set app namespace to ShirtBase

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace ShirtBase\Http\Controllers\Auth;
 
-use App\User;
+use ShirtBase\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use ShirtBase\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace ShirtBase\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use ShirtBase\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
