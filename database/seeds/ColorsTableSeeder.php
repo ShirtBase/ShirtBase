58ac66f13851209b1e548fdc22af22fc2d1ce0ce
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-10-04 12:44:57 +0200

    Add ColorsTableSeeder.

---
+++ database/seeds/ColorsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ColorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    // http://dixitpatel.com/how-to-seed-database-from-json-in-laravel/
+    public function run()
+    {
+        DB::table('colors')->truncate();
+
+        $colorsJsonSource = File::get(storage_path('seed_data/css-color-names.json'));
+        $colorsArray = json_decode($colorsJsonSource);
+
+        foreach ($colorsArray as $name => $hexCode) {
+            \ShirtBase\Color::create([
+                'name' => $name,
+                'hex_code' => $hexCode
+            ]);
+        }
+
+    }
+}
